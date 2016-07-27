232f0777075f3d8691448006050c5bd9fbc032da
阮佳佳 <250069802@qq.com>
2016-07-27 13:25:07 +0800

    ~

--- docker_nginx.sh
+++ docker_nginx.sh
@@ -4,5 +4,12 @@ current_dir=`echo $(pwd)/$(dirname $0)`;
 cd $current_dir;
 current_dir=$(pwd)
 echo $current_dir;
-docker run --name nginx -v $current_dir/www:/usr/share/nginx/html -p 8080:80 -d victorruan/nginx
+docker run \
+--name nginx \
+-v $current_dir/www:/usr/share/nginx/html \
+-v $current_dir/conf/nginx.conf:/etc/nginx/nginx.conf \
+-v $current_dir/conf/conf.d:/etc/nginx/conf.d \
+-v $current_dir/log/error.log:/var/log/nginx/error.log \
+-p 8080:80 \
+-d victorruan/nginx
 
