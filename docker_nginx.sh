2882884c55d010fdd1708f732710c8017f7475e4
阮佳佳 <250069802@qq.com>
2016-07-27 11:57:54 +0800

    ~

--- docker_nginx.sh
+++ docker_nginx.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 #进入文件所在目录
-cd $(pwd)/$(dirname $0);
-docker run --name nginx -v www:/usr/share/nginx/html -p 8080:80 -d victorruan/nginx
+current_dir=`echo $(pwd)/$(dirname $0)`;
+cd $current_dir;
+current_dir=$(pwd)
+echo $current_dir;
+docker run --name nginx -v $current_dir/www:/usr/share/nginx/html -p 8080:80 -d victorruan/nginx
 
